ec423afe5c66815bb98dc0b81ec68254e0d3a0f3
DaniyanP <54161489+DaniyanP@users.noreply.github.com>
2021-03-04 14:41:00 +0700

    up

---
+++ src/app/login/login.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { ExploreContainerComponentModule } from '../explore-container/explore-container.module';
+
+import { LoginPageRoutingModule } from './login-routing.module';
+import { LoginPage } from './login.page';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    ExploreContainerComponentModule,
+    RouterModule.forChild([{ path: '', component: LoginPage }]),
+    LoginPageRoutingModule
+  ],
+  declarations: [LoginPage]
+})
+export class LoginPageModule {}
